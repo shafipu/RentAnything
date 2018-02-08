477151009e4bba57d397a14d0cf80dbbeaf2b5b9
Admin <Admin@DESKTOP-BEVG5BN>
2018-02-08 09:55:25 +0530

    upadated Admin Pannel

--- RentAdminPannel/App_Start/BundleConfig.cs
+++ RentAdminPannel/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace RentAdminPannel
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/assets/js/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,8 +24,20 @@ namespace RentAdminPannel
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap_material").Include(
+                      "~/assets/js/bootstrap.min.js",
+                      "~/assets/jsbootstrap-notify.js",
+                      "~/assets/js/arrive.min.js",
+                      "~/assets/js/chartist.min.js",
+                      "~/assets/js/demo.js",
+                      "~/assets/js/material-dashboard.js",
+                      "~/assets/js/material.min.js",
+                      "~/assets/js/perfect-scrollbar.jquery.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/assets/css/material-dashboard.css",
+                      "~/assets/css/demo.css",
                       "~/Content/site.css"));
         }
     }
