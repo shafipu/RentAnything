28a228de26c7b70f24bf84627dffe47f1c975d06
Harleen <harleenk5911@gmail.com>
2018-02-13 12:24:26 +0530

    User Pages -Harleen Kaur

--- RentAdminPannel/Controllers/HomeController.cs
+++ RentAdminPannel/Controllers/HomeController.cs
@@ -16,18 +16,30 @@ namespace RentAdminPannel.Controllers
         {
             return View();
         }
-       /** public ActionResult About()
+        public ActionResult Item_display()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult Cart()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
-        }**/
+        }
+        public ActionResult Description()
+        {
+            return View();
+        }
+        /** public ActionResult About()
+         {
+             ViewBag.Message = "Your application description page.";
+
+             return View();
+         }
+
+         public ActionResult Contact()
+         {
+             ViewBag.Message = "Your contact page.";
+
+             return View();
+         }**/
     }
 }
