98e9aabd492809c07afbd6d2353129f7cd522abd
Harleen <harleenk5911@gmail.com>
2018-02-20 15:32:51 +0530

    Connecting Register with Database

--- RentAdminPannel/Models/AccountViewModels.cs
+++ RentAdminPannel/Models/AccountViewModels.cs
@@ -75,6 +75,7 @@ namespace RentAdminPannel.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -99,7 +100,6 @@ namespace RentAdminPannel.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-
         public string Code { get; set; }
     }
 
